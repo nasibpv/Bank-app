0b3096f722c7341157fa345780e69a36fc16ce5c
unknown <nasibnpv@gmail.com>
2023-02-01 09:01:08 +0530

    inital commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,19 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  // acno:any
   // acno=''
-  // password:any
+  acnumber:any
+  password:any
 
   // data="Your perfect banking partner"
 
-  inputplaceholder="Accound number"
+  // inputplaceholder="Accound number"
 
   userdetails:any={
     1000:{acno:1000,username:"anu",password:"abc123",balance:0},
-    1001:{acno:2000,username:"zayn",password:"abc123",balance:0},
-    1002:{acno:3000,username:"akil",password:"abc123",balance:0},
-    1003:{acno:4000,username:"nail",password:"abc123",balance:0},
+    1001:{acno:1001,username:"zayn",password:"abc123",balance:0},
+    1002:{acno:1002,username:"akil",password:"abc123",balance:0},
+    1003:{acno:1003,username:"nail",password:"abc123",balance:0},
   }
   constructor(){}
 
@@ -46,9 +46,36 @@ export class LoginComponent implements OnInit {
   // }
   // --------------------------------
 
-  login1(a:any,b:any){
-    var acnum=a.value
-    var paswd=b.value
+  // login1(a:any,b:any){
+  //   var acnum=a.value
+  //   var paswd=b.value
+  //   var userdetails1=this.userdetails
+
+  //   if(acnum in userdetails1){
+  //     if(paswd==userdetails1[acnum]["password"]){
+  //       alert("login success")
+  //     }
+  //     else{
+  //       alert("incurrect password")
+  //     }
+  //   }
+  //   else{
+  //     alert("acno incurrect or not registered")
+  //   }
+  // }
+
+  // acnoChange(event:any){
+  //   this.acno=event.target.value
+  //   // console.log(this.acno);
+  // }
+// ------------------------------------------
+
+
+login1(){
+    var acnum=this.acnumber
+    console.log(acnum);
+    
+    var paswd=this.password
     var userdetails1=this.userdetails
 
     if(acnum in userdetails1){
@@ -63,11 +90,7 @@ export class LoginComponent implements OnInit {
       alert("acno incurrect or not registered")
     }
   }
-
-  // acnoChange(event:any){
-  //   this.acno=event.target.value
-  //   // console.log(this.acno);
-  // }
+  // --------------------------------
 
   // passChange(event:any){
   //   this.password=event.target.value
